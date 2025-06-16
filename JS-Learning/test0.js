17c19aa34c877697c7187f6e2bba5ee9792d4916
daddy-ash25 <ashutoshkumar176644@gmail.com>
2025-06-16 18:11:24 +0000

    just some progress to studies

--- JS-Learning/test0.js
+++ JS-Learning/test0.js
@@ -1,2 +1,25 @@
-console.log("Here i am ...");
-console.log("Here i am once again ...");
+// console.log("Here i am ...");
+// console.log("Here i am once again ...");
+// let num = 50;
+// let Num = 100;
+// let valueInNumber = Number(50);
+// console.log(typeof num);
+
+
+
+// let mess=1+2+"3"+"4"+5+6
+// console.log(mess);
+// console.log(typeof mess);
+
+// let num = new Number(200.15555);
+// console.log(typeof num);
+// console.log(num);
+
+// console.log(num.toFixed(3));
+
+
+let mydate = new Date();
+console.log(mydate.toLocaleString());
+
+
+
