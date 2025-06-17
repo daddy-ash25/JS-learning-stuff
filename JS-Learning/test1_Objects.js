f335e02069d3db0ff4b6c6c884111501d698bd7f
daddy-ash25 <ashutoshkumar176644@gmail.com>
2025-06-17 17:28:20 +0000

    just learned about objects andall

---
+++ JS-Learning/test1_Objects.js
@@ -0,0 +1,28 @@
+// const heroes = {
+//     ironman : "tony stark",
+//     batman : "brus vayn",
+//     "black panther" : "tachala",
+// }
+// console.log(heroes.batman);
+// console.log(heroes["black panther"]);
+
+const obj1 = {a:"23", b :"5"};
+const obj2 = {c:"2", d :"9"};
+const obj3 = {
+    e:"3",
+    f :"34",
+    g : {
+        h :"56",
+    },
+};
+
+// const obj4 = Object.assign(obj1,obj2,obj3)  fine but not recemended
+// const obj4 = Object.assign({},obj1,obj2,obj3)   do not disturb any of the objects
+//                           obj 1 acts as the destination so it is better to give a empty bracket in the start
+
+
+const obj4 = {...obj1,...obj2,...obj3} // ...spreads the object or the array into their individual elements
+console.log(obj4);
+console.log(obj1);
+
+ 
