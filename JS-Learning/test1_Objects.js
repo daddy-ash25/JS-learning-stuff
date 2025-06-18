f018896bd051f0abcce1ca41570c37c7f6dd34db
daddy-ash25 <ashutoshkumar176644@gmail.com>
2025-06-18 07:06:13 +0000

    nothing special at all

--- JS-Learning/test1_Objects.js
+++ JS-Learning/test1_Objects.js
@@ -6,23 +6,34 @@
 // console.log(heroes.batman);
 // console.log(heroes["black panther"]);
 
-const obj1 = {a:"23", b :"5"};
-const obj2 = {c:"2", d :"9"};
-const obj3 = {
-    e:"3",
-    f :"34",
-    g : {
-        h :"56",
-    },
-};
+// const obj1 = {a:"23", b :"5"};
+// const obj2 = {c:"2", d :"9"};
+// const obj3 = {
+//     e:"3",
+//     f :"34",
+//     g : {
+//         h :"56",
+//     },
+// };
 
 // const obj4 = Object.assign(obj1,obj2,obj3)  fine but not recemended
 // const obj4 = Object.assign({},obj1,obj2,obj3)   do not disturb any of the objects
 //                           obj 1 acts as the destination so it is better to give a empty bracket in the start
 
 
-const obj4 = {...obj1,...obj2,...obj3} // ...spreads the object or the array into their individual elements
-console.log(obj4);
-console.log(obj1);
+// const obj4 = {...obj1,...obj2,...obj3} // ...spreads the object or the array into their individual elements
+// console.log(obj4);
+// console.log(obj1);
 
- 
+
+let num = 50;
+
+let num1 = function(anumber){
+    let temp = anumber-100
+    return temp;
+}
+
+function afunction(anumber){
+    return anumber+1
+}
+
